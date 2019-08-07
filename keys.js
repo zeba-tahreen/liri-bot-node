38beddda1cf8d20e493eedcd7e61486161b6118f
tahreen zeba <tahreenzeba1@gmail.com>
2019-08-07 19:26:22 -0400

    Fixed the spotify commant with minor syntax errors

--- keys.js
+++ keys.js
@@ -2,5 +2,5 @@ console.log("This Is Loaded");
 
 exports.spotify = {
     id: process.env.SPOTIFY_ID,  
-    secret : process.env.SPOTIFY_SECERET
+    secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,13 +1,14 @@
-require("dotenv").config();   
+require("dotenv").config();
+
 
 // the variables
 var fs = require("fs");                        //  console.log(fs);
 var keys = require("./keys.js");                // console.log(keys);
-var Spotify = require("spotify-web-api-node");  //console.log(Spotify);
+var Spotify = require("node-spotify-api");  //console.log(Spotify);
 var axios = require("axios");                   // console.log(axios);
 var moment = require("moment");                // console.log(moment);
-
 var spotify = new Spotify(keys.spotify);       // console.log(spotify);
+
 var command = process.argv[2];                //  console.log(command);
 var arg = process.argv;                        // console.log(arg);
 var reference = [];                            // console.log(reference);
@@ -17,146 +18,133 @@ var theBand = "";                               //console.log(theBand);
 var filename = "log.txt";                       //console.log(filename);
 var fullCommand = [];                           // console.log(command);
 
+
 // getting reference (user choice) to accept several words
 
-for (var i=3; i < arg.length; i++){
+for (var i = 3; i < arg.length; i++) {
     reference.push(arg[i])
 }
-var referenceBand = reference.join("");
+var reference = reference.join("");
+console.log(reference)
 // End of reference
 
-// loggin full command
-fullCommand.push(command);
-if(reference.length !=0){
-    fullCommand.push(referenceBand);
-}
+console.log(fullCommand);
 
 //logging function
-function logging(value){
-    fs.appendFile(filename,","+ value, function (err){
-        if (err){
+function logging(value) {
+    fs.appendFile(filename, value, function (err) {
+        if (err) {
             return console.log("It's an error")
         }
     })
 }
-logging(fullCommand);
 
-
+CheckCommand(command, reference)
 // commands:
 // concert-this, spotify-this-song, movie-this, do-what-it-says
-
-switch (command) {
-
+function CheckCommand(command, reference) {
+    var fullCommand = command + "," + reference + ",";
+    switch (command) {
         case "concert-this":
-            concert(referenceBand);
+            logging(fullCommand);
+            concert(reference);
             break;
         case "spotify-this-song":
-            spotifySong(command);
+            logging(fullCommand);
+            spotifySong(reference);
             break;
         case "movie-this":
+            logging(fullCommand);
             movie(reference);
             break;
         case "do-what-it-says":
             doThat();
-        break;
+            break;
     };
-
+}
 // concert-this function
 
-function concert(referenceBand){
+function concert(referenceBand) {
     var bandUrl = "https://rest.bandsintown.com/artists/" + referenceBand + "/events?app_id=codingbootcamp&limit=5";
     axios.get(bandUrl).then(
-        function (response){
+        function (response) {
             console.log("  ")
             console.log("==== Your Choice Band/Artist Info :  " + referenceBand + " ==== ==== ==== ");
-            for(var i = 0; i < response.data.length; i++){
+            for (var i = 0; i < response.data.length; i++) {
                 var datetime = response.data[i].datetime; // saves datetime response into and array
 
                 var dateArr = datetime.split("T"); // splits the date and tie in the response
 
                 var concertResults =
-                "==== ==== ==== ====" +
-                "\nVenue Name : "+ response.data[i].venue.name +
-                "\nVenue Location : "+ response.data[i].venue.city +
-                "\nDate of the Event :"+ moment(dateArr[0], "YYYY-DD-MM").format("DD/MM/YYYY");
+                    "==== ==== ==== ====" +
+                    "\nVenue Name : " + response.data[i].venue.name +
+                    "\nVenue Location : " + response.data[i].venue.city +
+                    "\nDate of the Event :" + moment(dateArr[0], "YYYY-DD-MM").format("DD/MM/YYYY");
                 // and it changes to a new format
                 console.log(concertResults);
-            }   console.log("  ");
-                console.log("==== ==== ==== ====");
-                console.log("  ");
+            } console.log("  ");
+            console.log("==== ==== ==== ====");
+            console.log("  ");
 
         })
-        .catch(function(error){
+        .catch(function (error) {
             console.log("This is the error: " + error);
         });
 };
 // movie-this Function
 
-function movie(reference){
-    if(!reference){
+function movie(reference) {
+    if (!reference) {
         reference = "mr nobody";
     }
     axios.get("http://www.omdbapi.com/?t=" + reference + "&y=&plot=short&tomatoes=true&apikey=trilogy")
-    .then(function(response){
-        var rotten = response.data.Ratings[1]
-        // cosnole.log("this is the rotten value :" + rotten)
-        if(rotten === undefined){
-            rotten = "Not Available"
-        }else{
-            rotten = response.data.Ratings[1].value;
-        }
-        console.log(" ");
-        console.log("===== Movie Information ===== " + response.data.Title + "==== ====");
-        console.log(" ");
-        
-        var movieResults =
-        "\n Title : " + response.data.Title +
-        "\n Year : " + response.data.Year +
-        "\n IMDB Rating : " + response.data.Rated +
-        "\n Rotten Tomatoes Ratting : " + rotten +
-        "\n Country Produced : " + response.data.Country +
-        "\n Language : " + response.data.Language +
-        "\n Plot : " + response.data.Plot +
-        "\n Actors : " + response.data +
-        "\n " +
-        "\n===========" +
-        "\n ";
-        console.log(movieResults);
+        .then(function (response) {
+            var rotten = response.data.Ratings[1]
+            // cosnole.log("this is the rotten value :" + rotten)
+            if (rotten === undefined) {
+                rotten = "Not Available"
+            } else {
+                rotten = response.data.Ratings[1].value;
+            }
+            console.log(" ");
+            console.log("===== Movie Information ===== " + response.data.Title + "==== ====");
+            console.log(" ");
 
-    })
-    .catch(function(error){
-        console.log("This is the " + error);
-    });
+            var movieResults =
+                "\n Title : " + response.data.Title +
+                "\n Year : " + response.data.Year +
+                "\n IMDB Rating : " + response.data.Rated +
+                "\n Rotten Tomatoes Ratting : " + rotten +
+                "\n Country Produced : " + response.data.Country +
+                "\n Language : " + response.data.Language +
+                "\n Plot : " + response.data.Plot +
+                "\n Actors : " + response.data +
+                "\n " +
+                "\n===========" +
+                "\n ";
+            console.log(movieResults);
+
+        })
+        .catch(function (error) {
+            console.log("This is the " + error);
+        });
 
 }
-movie();
 
 // do what it says function
-function doThat(){
-    fs.readFile("random.txt","utf8", function(error,data){
-        if(error){
+function doThat() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
             return console.log(error);
         }
         var dataArr = data.split(",");
         console.log(" ");
         console.log("--------- menu of content -------");
         console.log(" ")
-        for(var i=0; i < dataArr.length; i++){
-            if(dataArr[i] === "spotify-this-song"){
-                theSong = dataArr[++i];
-                console.log("----- spotify song"+ theSong + "-----")
-                spotifySong(theSong);
-
-            }else if (dataArr[i] === "movie-this"){
-                theMovie= dataArr[++i];
-                console.log("------ movie this ----- " + theMovie+"  -------")
-                movie(theMovie);
-            }else if(dataArr[i] === "concert-this"){
-                theBand= dataArr[++i];
-                console.log("----- the bands----- " + theBand+ "-----")
-                concert(theBand);
-            }else{
-                console.log("Sorry..., This command is not accepted");
+        for (var i = 0; i < dataArr.length; i++) {
+            if (i % 2 === 0) {
+                console.log(dataArr[i], dataArr[i + 1])
+                CheckCommand(dataArr[i], dataArr[i+ 1])
             }
         }
     })
@@ -164,21 +152,20 @@ function doThat(){
 
 // Spotify-this-song function
 
-function spotifySong(reference){
-    if(reference.length === 0 ){
-        reference = "The Sign";
+function spotifySong(song) {
+    if (song) {
+        song = "The Sign";
     }
-    spotify.search({type: "track", query: reference})
-    .then(function(response){
+    spotify.search({ type: "track", query: song }, function (err, response) {
         console.log(" ");
-        console.log("==== Playing Spotify ====" + reference + " ==== ");
+        console.log("==== Playing Spotify ====" + song + " ==== ");
         console.log(" ");
-        for(var i =0; i < 5; i ++){
+        for (var i = 0; i < 5; i++) {
             var spotifyResults = "==== ==== ==== ====" +
-            "\nArtist(s): " + response.tracks.items[i].artist[0].name +
-            "\nSong Name: " + response.tracks.items[i].name +
-            "\nAlbum Name: " + response.tracks.items[i].album.name +
-            "\nPreview Link: " + response.tracks.items[i].preview_url;
+                "\nArtist(s): " + response.tracks.items[i].artists[0].name +
+                "\nSong Name: " + response.tracks.items[i].name +
+                "\nAlbum Name: " + response.tracks.items[i].album.name +
+                "\nPreview Link: " + response.tracks.items[i].preview_url;
 
             console.log(spotifyResults);
         }
@@ -186,10 +173,7 @@ function spotifySong(reference){
         console.log("==== ==== ==== ====");
         console.log(" ");
     })
-    .catch(function(err){
-        console.log(err);
-    })
-    
+
 };
 
 
